ec84edf3078984e5d986c3dd4b99a7267e6b12bb
reinouthza <reinouthza@users.noreply.github.com>
2025-01-28 13:26:19 +0000

    Code quality VolumeSlider

---
+++ main.py
@@ -0,0 +1,16 @@
+package nonRelational
+
+import java.io.File
+
+fun main() {
+    val fileName = "main"
+    println("Processing ${fileName} in nonRelational")
+
+    val file = File(fileName)
+    if (file.exists()) {
+        val lines = file.readLines()
+        println("Read ${lines.size} lines")
+    } else {
+        println("File does not exist")
+    }
+}
